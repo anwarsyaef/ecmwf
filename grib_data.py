b29d67e7d51dcf404eb633620a76da8f2c570347
tpk@tb3 <tpkludka@gmail.com>
2017-06-20 10:05:44 +0100

    extract csv

--- grib_data.py
+++ grib_data.py
@@ -1,18 +1,44 @@
 import pandas as pd
-from datetime import datetime, timedelta
 
-save_path = "/home/tpk/projects/ecmwf/csv/"
-temp_path = "/home/tpk/projects/ecmwf/csv/ARAD_T.csv"
+stations_path = "/home/tpk/projects/ecmwf/stations.txt"
+in_path = "/home/tpk/projects/ecmwf/csv/"
+out_path = "/home/tpk/projects/ecmwf/csv/daily/"
+
+K = 273.15
+
+txt_stations = pd.read_csv(stations_path, header=None, delimiter=';')
+
+for suffix in ['_T','_P']:
+    for index, row in txt_stations.iterrows():
+        name = row[0]
+        path = in_path + name + suffix + '.csv'
+        data = pd.read_csv(path, delimiter=';')
+        data['datetime'] = pd.to_datetime(data['datetime'], format='%Y-%m-%d %H:%M:%S')        
+        if suffix == '_P':
+            data['datetime'] = data['datetime'] + pd.DateOffset(hours=-1)
+            data.insert(1,'date',data['datetime'].dt.date)
+            data.insert(2,'hour',data['datetime'].dt.hour)
+            # df.loc[df['column_name'] == some_value]
+            data = data.loc[data['hour'].isin([11,23])]
+            daily = data.groupby('date').sum()
+            daily = daily['precip'] * 1000
+        else:
+            data.insert(1,'date',data['datetime'].dt.date)
+            # daily = data.groupby('date').mean()
+            daily = data.groupby('date').min()
+            daily = daily['temp'] - K
+        daily.to_csv(out_path + name + suffix + ".csv", sep=';')
+        print(name + suffix, ' daily data saved')
 
 
-data = pd.read_csv(temp_path, delimiter=';')
 
-
-
-data['datetime'] = pd.to_datetime(data['datetime'], format='%Y-%m-%d %H:%M:%S')
-data['datetime'][0].type
-
-# data.insert(1, dftest['datetime'].
-#df.insert(idx, col_name, value)
-
-    
+#data = pd.read_csv("/home/tpk/projects/ecmwf/csv/CELJE_P.csv", delimiter=';')
+#data['datetime'] = pd.to_datetime(data['datetime'], format='%Y-%m-%d %H:%M:%S')        
+#import matplotlib.pyplot as plt    
+#import numpy as np
+#dp = data['precip'].loc[:47]
+#fig, ax = plt.subplots()
+#ax.plot(dp)
+#ax.set_xticks(np.arange(-1, 47, 4))
+#ax.xaxis.grid(True, which='major')
+#plt.show()
